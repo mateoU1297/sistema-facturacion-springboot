1c3954e9fa9e49d9d4c6bd285c377653bc7bc7fb
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-02 22:06:21 -0500

    se implementa la carga de archivos

---
+++ src/main/java/com/udemy/springboot/di/app/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.udemy.springboot.di.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+	}
+	
+	
+
+}
--- src/main/java/com/udemy/springboot/di/app/models/entity/Cliente.java
+++ src/main/java/com/udemy/springboot/di/app/models/entity/Cliente.java
@@ -43,6 +43,8 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 
+	private String foto;
+	
 	public Long getId() {
 		return id;
 	}
@@ -81,6 +83,14 @@ public class Cliente implements Serializable {
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 
 	public static long getSerialversionuid() {
