68711813e912f1d980ce6224bb4dd1d91d03bf28
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-09 23:12:01 -0500

    se acaba de implementar traducciones en las vistas y controladores

--- src/main/java/com/udemy/springboot/di/app/controllers/LoginController.java
+++ src/main/java/com/udemy/springboot/di/app/controllers/LoginController.java
@@ -1,7 +1,10 @@
 package com.udemy.springboot.di.app.controllers;
 
 import java.security.Principal;
+import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,22 +13,25 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+    private MessageSource messageSource;
 
 	@GetMapping("/login")
 	public String login(@RequestParam(required = false) String error, @RequestParam(required = false) String logout,
-			Model model, Principal principal, RedirectAttributes flash) {
+			Model model, Principal principal, RedirectAttributes flash, Locale locale) {
 
-		if (principal != null) {
-			flash.addFlashAttribute("info", "Ya ha iniciado sesión anteriormente");
+		if(principal != null) {
+			flash.addFlashAttribute("info", messageSource.getMessage("text.login.already", null, locale));
 			return "redirect:/";
 		}
-
-		if (error != null) {
-			model.addAttribute("error", "Nombre de usuario o contraseña incorrectos");
+		
+		if(error != null) {
+			model.addAttribute("error", messageSource.getMessage("text.login.error", null, locale));
 		}
 		
 		if(logout != null) {
-			model.addAttribute("success", "Ha cerrado sesión con éxito!");
+			model.addAttribute("success", messageSource.getMessage("text.login.logout", null, locale));
 		}
 
 		return "login";
