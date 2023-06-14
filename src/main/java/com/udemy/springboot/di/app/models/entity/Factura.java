29273fad48a8ea28f1194634a3a8487f266007ec
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-13 23:26:30 -0500

    se agrega dependencia para convertir vistas en archivos xml

--- src/main/java/com/udemy/springboot/di/app/models/entity/Factura.java
+++ src/main/java/com/udemy/springboot/di/app/models/entity/Factura.java
@@ -20,34 +20,32 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.NotEmpty;
-
+import jakarta.xml.bind.annotation.XmlTransient;
 
 @Entity
-@Table(name="facturas")
+@Table(name = "facturas")
 public class Factura implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotEmpty
 	private String descripcion;
-	
+
 	private String observacion;
-	
+
 	@Temporal(TemporalType.DATE)
 	@Column(name = "create_at")
 	private Date createAt;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
-	
+
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "factura_id")
 	private List<ItemFactura> items;
-	
-	
-	
+
 	public Factura() {
 		this.items = new ArrayList<ItemFactura>();
 	}
@@ -89,6 +87,7 @@ public class Factura implements Serializable {
 		this.createAt = createAt;
 	}
 
+	@XmlTransient
 	public Cliente getCliente() {
 		return cliente;
 	}
@@ -104,20 +103,20 @@ public class Factura implements Serializable {
 	public void setItems(List<ItemFactura> items) {
 		this.items = items;
 	}
-	
+
 	public void addItemFactura(ItemFactura item) {
 		this.items.add(item);
 	}
-	
+
 	public Double getTotal() {
 		Double total = 0.0;
-		
+
 		int size = items.size();
-		
-		for(int i=0; i < size; i++) {
+
+		for (int i = 0; i < size; i++) {
 			total += items.get(i).calcularImporte();
 		}
-		
+
 		return total;
 	}
 
